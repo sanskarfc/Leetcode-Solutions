4ee24ab907cea384de65f1e33814a2d2cc73cc85
sanskarfc <72058551+sanskarfc@users.noreply.github.com>
2022-07-10 15:11:29 +0530

    Time: 341 ms (77.50%), Space: 73.5 MB (94.05%) - LeetHub

---
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
+        set<pair<int,int>> s1;
+        for(int i = 0; i < (int)logs.size(); i++){
+            s1.insert({logs[i][0], logs[i][1]});
+        }
+        vector<int> v1;
+        for(auto p : s1){
+            v1.push_back(p.first);
+        }
+        vector<int> ans(k,0);
+        int count = 0;
+        int prev = v1[0];
+        cout << "v1 --> ";
+        for(int i = 0; i < (int)v1.size(); i++){
+            cout << v1[i] << " ";
+            if(prev == v1[i]){
+                count++;
+            }
+            else{
+                ans[count - 1]++;
+                count = 1;
+                prev = v1[i];
+            }
+        }
+        cout << endl;
+        ans[count - 1]++;
+        return ans;
+    }
+};
