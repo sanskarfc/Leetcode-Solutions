92e3857c788f8bdb3970c2d8719d783eb6b7124d
sanskarfc <72058551+sanskarfc@users.noreply.github.com>
2022-07-13 12:08:06 +0530

    Time: 814 ms (74.24%), Space: 171.7 MB (63.13%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -1,39 +1,44 @@
-// this is not my code, i am just checking if this works or not, because i did exactly this and it did not pass :tear:
+// bruh this took too long, i hate this
 
 class Solution {
-    typedef array<int, 26> T;
 public:
-    int largestPathValue(string C, vector<vector<int>>& E) {
-        unordered_map<int, vector<int>> G;
-        vector<int> indegree(C.size());
-        for (auto &e : E) {
-            G[e[0]].push_back(e[1]); // build graph
-            indegree[e[1]]++; // count indegrees
+    int largestPathValue(string C, vector<vector<int>>& edges) {
+        int n = C.length();
+        int m = edges.size();
+        int ans = 0;
+        vector<vector<int>> g(n + 1);
+        vector<vector<int>> dp(n + 1, vector<int>(26,0));
+        vector<int> indeg(n + 1, 0);
+        vector<int> order; //Stores the Topological Order
+        
+        for (auto &e : edges) {
+            g[e[0]].push_back(e[1]); // build graph
+            indeg[e[1]]++; // count indegrees
         }
-        vector<T> cnt(C.size(), T{}); // cnt[i][j] is the maximum count of j-th color from the ancester nodes to node i.
+        
         queue<int> q;
-        for (int i = 0; i < C.size(); ++i) {
-            if (indegree[i] == 0) { // if this node has 0 indegree, we can use it as a source node
+        for(int i = 0; i < n; i++){
+            if(indeg[i] == 0){
                 q.push(i);
-                cnt[i][C[i] - 'a'] = 1; // the count of the current color should be 1
+                dp[i][C[i] - 'a'] = 1;
             }
         }
-        int ans = 0, seen = 0;
-        while (q.size()) {
-            auto u = q.front();
+        
+        ans = 0;
+        
+        while(!q.empty()){
+            int u = q.front();
             q.pop();
-            int val = *max_element(begin(cnt[u]), end(cnt[u])); // we use the maximum of all the maximum color counts as the color value.
-            ans = max(ans, val);
-            ++seen;
-            for (int v : G[u]) {
-                for (int i = 0; i < 26; ++i) {
-                    cnt[v][i] = max(cnt[v][i], cnt[u][i] + (i == C[v] - 'a')); // try to use node `u` to update all the color counts of node `v`.
+            order.push_back(u);
+            for(int i = 0; i < 26; i++) ans = max(ans, dp[u][i]);
+            for(auto v : g[u]){
+                for(int i = 0; i < 26; i++){
+                    dp[v][i] = max(dp[v][i], dp[u][i] + (C[v] - 'a' == i));
                 }
-                if (--indegree[v] == 0) {
-                    q.push(v);
-                }
+                indeg[v]--;
+                if(!indeg[v]) q.push(v);
             }
         }
-        return seen < C.size() ? -1 : ans;
+        return ((int)order.size() == n) ? ans : -1;
     }
 };
