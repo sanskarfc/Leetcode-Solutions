d5a72106c94a8638bdb60996a9a98285661302b7
sanskarfc <72058551+sanskarfc@users.noreply.github.com>
2022-07-13 15:16:59 +0530

    Time: 217 ms (24.52%), Space: 36.7 MB (18.39%) - LeetHub

--- 975-odd-even-jump/975-odd-even-jump.cpp
+++ 975-odd-even-jump/975-odd-even-jump.cpp
@@ -8,26 +8,20 @@ public:
         dp[n - 1][1] = true;
         s1.insert({arr[n - 1], n - 1});
         for(int i = n - 2; i >= 0; i--){
-            cout << "curr: " << arr[i] << endl;
             auto it = s1.lower_bound({arr[i],0});
             if(it != s1.end()){
                 dp[i][1] = dp[(*it).second][0];
             }
             it = s1.upper_bound({arr[i],INT_MAX});
             if(it == s1.begin()){
-                cout << "begin :[" << endl;
                 dp[i][0] = false;
             }
             else{
                 it--;
                 int val = (*it).first;
-                cout << "val: " << val << endl;
                 it = s1.lower_bound({val,0});
                 dp[i][0] = dp[(*it).second][1];
             }
-            cout << "dp[i][0]: " << dp[i][0] << endl;
-            cout << "dp[i][1]: " << dp[i][1] << endl;
-            cout << endl;
             s1.insert({arr[i],i});
         }
         int ans = 0;
